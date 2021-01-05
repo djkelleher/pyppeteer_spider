e16f91acf7a2d8f384e386bfe7100148e9f56234
Dan <kelleherjdan@gmail.com>
2021-01-05 10:41:21 -0500

    change docker base image

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name='distbot',
-    version='2.8.5',
+    version='2.8.6',
     author='Dan Kelleher',
     author_email='danielkelleher@protonmail.com',
     maintainer='Dan Kelleher',
